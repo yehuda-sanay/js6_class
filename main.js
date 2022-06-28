efeb7a3915b854f2cbf965f18f8e390933b9c4ed
yehuda-sanay <yehudasanay1@gmail.com>
2022-06-28 23:02:48 +0300

    28.06.22

--- main.js
+++ main.js
@@ -14,29 +14,43 @@
 // console.log(student2);
 
 //שאלה 3
-let tableh=document.getElementById("user_thead");
-let tableb=document.getElementById("user_tbody");
-let vll = document.getElementById("try");
-function UsersDitels(){
-class Useres {
-    constructor(firstName,lastName,age,email){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.age=age;
-        this.email=email;
-    }
+// let tableh=document.getElementById("user_thead");
+// let tableb=document.getElementById("user_tbody");
+// let vll = document.getElementById("try");
+// function UsersDitels(){
+// class Useres {
+//     constructor(firstName,lastName,age,email){
+//         this.firstName=firstName;
+//         this.lastName=lastName;
+//         this.age=age;
+//         this.email=email;
+//     }
+// }
+// const user1=new Useres("yehdua","sanay",31,"yehudasanay1@gmail.com");
+// const user2=new Useres("aviv","tadela",31,"aviv1@gmail.com");
+// const user3=new Useres("nadav","talala",32,"nadavt@gmail.com");
+// // console.log(user1,user2,user3);
+// const user4=new Useres(user_firstname.value,user_lastname.value,user_age.value,user_email.value);
+// console.log(user4);
+
+// for(const key in user1){
+//     vll.innerHTML+=`<th>${user1[key]}</th>`;
+// }
+
+// }
+
+class Dog{
+    constructor(dogName,dogBreed,dateOfBirth){
+    this.dogName=dogName;
+    this.dogBreed=dogBreed;
+    this.dateOfBirth=dateOfBirth;
 }
-const user1=new Useres("yehdua","sanay",31,"yehudasanay1@gmail.com");
-const user2=new Useres("aviv","tadela",31,"aviv1@gmail.com");
-const user3=new Useres("nadav","talala",32,"nadavt@gmail.com");
-// console.log(user1,user2,user3);
-const user4=new Useres(user_firstname.value,user_lastname.value,user_age.value,user_email.value);
-console.log(user4);
-
-for(const key in user1){
-    vll.innerHTML+=`<th>${user1[key]}</th>`;
 }
+const dog1=new Dog("aviv","lavrador","16.02");
+const dog2=new Dog("nadav","pitbul","15.03");
+const dog3=new Dog("danny","german","22.06");
+const dog4=new Dog("roy","border","02.03");
+const dog5=new Dog("shaked","koly","12.11");
+const dogArray=[dog1,dog2,dog3,dog4,dog5];
 
 
-
-}
